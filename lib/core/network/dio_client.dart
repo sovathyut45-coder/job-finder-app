b8fddaf9f1e455d0b6a9ae076acabc9b9c458006
Sovath <sovathyut45@gmail.com>
2026-06-15 21:52:09 +0700

    Integrate Adzuna API and dotenv config
    Switch API provider from RapidAPI to Adzuna: replace base URL and RapidAPI key usage with a new ApiConstant that reads ADZUNA_APP_ID and ADZUNA_APP_KEY from flutter_dotenv. Update Dio client to set Accept header and inject app_id/app_key as query parameters via an interceptor. Adapt job datasource to Adzuna response shape (use /gb/search/{page}, what/results_per_page) and map Adzuna fields to the app's job model. Add flutter_dotenv to pubspec and include the .env asset for environment configuration.

--- lib/core/network/dio_client.dart
+++ lib/core/network/dio_client.dart
@@ -4,13 +4,21 @@ import 'package:job_finder_app/core/constants/api_constants.dart';
 class DioClient {
   static final Dio dio = Dio(
     BaseOptions(
-      baseUrl: ApiConstants.baseUrl,
-      connectTimeout: const Duration(seconds: 30),
-      receiveTimeout: const Duration(seconds: 30),
+      baseUrl: ApiConstant.baseUrl,
       headers: {
-        'X-RapidAPI-Key': ApiConstants.apiKey,
-        'X-RapidAPI-Host' : 'jsearch.p.rapidapi.com'
-      }
-    )
-  );
+        'Accept': 'application/json',
+      },
+    ),
+  )..interceptors.add(
+      InterceptorsWrapper(
+        onRequest: (options, handler) {
+          options.queryParameters.addAll({
+            'app_id': ApiConstant.appId,
+            'app_key': ApiConstant.appKey,
+          });
+
+          handler.next(options);
+        },
+      ),
+    );
 }
--- lib/data/datasource/job_datasource.dart
+++ lib/data/datasource/job_datasource.dart
@@ -1,28 +1,59 @@
 import 'package:job_finder_app/core/network/dio_client.dart';
 
 class JobDatasource {
-Future<List<dynamic>> getJobs({
-  required String query,
-  int page = 1,
-  int numPages = 1,
-}) async {
-  //final stopwatch = Stopwatch()..start();
-  final response = await DioClient.dio.get(
-    '/search',
-    queryParameters: {
-      'query': query,
-      'page': page,
-      'num_pages': numPages,
-    },
-  );
+  Future<List<dynamic>> getJobs({
+    required String query,
+    int page = 1,
+  }) async {
 
-    // stopwatch.stop();
+    final response = await DioClient.dio.get(
+      '/gb/search/$page',
+      queryParameters: {
+        'what': query,
+        'results_per_page': 20,
+      },
+    );
 
-    // print('Detail API: ${stopwatch.elapsedMilliseconds} ms');
-  
-  return response.data['data'];
-  
-}
+    final List jobs = response.data['results'];
+
+    return jobs.map(
+      (job) => {
+        'job_id': job['id']?.toString() ?? '',
+
+        'job_title': job['title'] ?? '',
+
+        'employer_name':
+            job['company']?['display_name'] ?? '',
+
+        'employer_logo': '',
+
+        'job_location':
+            job['location']?['display_name'] ?? '',
+
+        'job_city':
+            job['location']?['area'] != null &&
+                    (job['location']['area'] as List).isNotEmpty
+                ? job['location']['area'].last
+                : '',
+
+        'job_country': '',
+
+        'job_employment_type': '',
+
+        'job_description':
+            job['description'] ?? '',
+
+        'job_apply_link':
+            job['redirect_url'] ?? '',
+
+        'job_min_salary':
+            job['salary_min'] ?? 0,
+
+        'job_max_salary':
+            job['salary_max'] ?? 0,
+      },
+    ).toList();
+  }
 
 
   Future<List<dynamic>> getJobDetail(
